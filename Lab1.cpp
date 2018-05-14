d0482b27ad229ee785ca71ab0d6303c321b34d37
celestepimm <celestepimm@gmail.com>
2018-05-14 14:14:41 -0400

    working mesh

--- Lab1.cpp
+++ Lab1.cpp
@@ -19,9 +19,8 @@ using namespace std;
 
 
 
-
 // Window dimensions
-const GLuint WIDTH = 1024, HEIGHT = 1024;
+const GLuint windowWidth = 800, windowHeight = 800;
 
 glm::vec3 triangle_scale;
 glm::vec3 camera_translation = glm::vec3(0.0f, 0.0f, -1.0f);
@@ -38,6 +37,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 }
 
+
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
@@ -51,7 +51,7 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Triangle", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Heightmap", nullptr, nullptr);
 	if (window == nullptr)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -156,20 +156,102 @@ int main()
 
 	glUseProgram(shaderProgram);
 
-	GLfloat vertices[] = {
-		0.0f, 0.5f, 0.0f,  // Top
-		0.5f, -0.5f, 0.0f,  // Bottom Right
-		-0.5f, -0.5f, 0.0f,  // Bottom Left
-	};
+
+	triangle_scale = glm::vec3(1.0f/100, 1.0f/100, 1.0f); //shorthand, initializes all 4 components to 1.0f
+
+
+	//change of coordinates between model 
+
+	
+
+	//IMAGE/TEXTURE PROCESSING
+	//loading the image 
+	int nrChannels;
+	unsigned char *data = stbi_load("bwimage.png", &width, &height, &nrChannels, 0);
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+
+	//text filtering options
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	//test image size is within contstraints, else exit
+	if (width > 1024 || height > 1024 || width < 256 || height < 256)
+	{
+		std::cout << "failed to load image because it is not within size constraints. Image is sized:" << height << " " << width << std::endl;
+		exit(0);
+	}
+	if (data)
+	{	//turn image into a texter
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		std::cout << "successful load" << std::endl;
+		std::cout << height << std::endl;
+		std::cout << width << std::endl;
+	}
+	else
+	{
+		std::cout << "failed to load texture" << std::endl;
+	}
+
+	int numtriangles = ((width - 1)*(height - 1) * 2); //see pixel index notes
+	int numvertices = numtriangles * 3;
+	GLfloat *vertices = new GLfloat[numvertices*3]; // allocate memory dynamically 
+
+	//looping through image to add to VBO 
+
+	for (int i=0; i < width-1; i++) //exclude last row
+	{
+		for (int j = 0; j< height-1; j++) // exclude last column 
+		{
+			int heightmapvalue= data[(width*i)+j];  // get each individual colour value 
+			
+			float v[] = {
+				data[(width*i) + j]/255.0,
+				data[((width*i) + j) + 1]/255.0,
+				data[((width*(i + 1)) + j)]/255.0, // add 1 to i 
+				data[((width*(i + 1)) + j) + 1]/255
+			};
+
+			float x = i - (0.5*width); // centers in window
+
+			float y = j - (0.5*height);
+
+
+			GLfloat pixelvertices[] =
+			{
+				x, y, v[0], // first triangle 
+				x+1, y, v[1], 
+				x, y+1, v[2], 
+				x+1, y+1, v[3],
+				x, y + 1, v[2],
+				x + 1, y, v[1], // fix order
+
+			};
+
+			//float z = (heightmapvalue / 255.0); // make a floating point between 0 and 1
+			for (int k = 0; k < sizeof(pixelvertices) / sizeof(GLfloat); k++)
+			{
+
+				vertices[(((width-1)*i) + j) *18+k ]= pixelvertices[k]; // coordinates per vertex times number of vertices in two triangles + k (relative offset)
+				
+			}
+			//std::cout << heightmapvalue << std::endl;
+		}
+	}
+
+	
 
 	GLuint VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	//glGenBuffers(2, &VBO);
+
 	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (numvertices*3)*sizeof(GLfloat), vertices, GL_STATIC_DRAW); // multiply by the size of GL float (4 bits) because Specifies the size in bytes of the buffer object's new data store.
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
@@ -178,11 +260,14 @@ int main()
 
 	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
 
-	triangle_scale = glm::vec3(1.0f); //shorthand, initializes all 4 components to 1.0f
-
 	GLuint transformLoc = glGetUniformLocation(shaderProgram, "model_matrix");
 	GLuint viewMatrixLoc = glGetUniformLocation(shaderProgram, "view_matrix");
 	GLuint projectionLoc = glGetUniformLocation(shaderProgram, "projection_matrix");
+
+
+	//free image memory
+	stbi_image_free(data);
+
 
 	// Game loop
 	while (!glfwWindowShouldClose(window))
@@ -195,57 +280,29 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//glm::mat4 model_matrix;
-		//model_matrix = glm::scale(model_matrix, triangle_scale);
+		glm::mat4 model_matrix;
+		model_matrix = glm::scale(model_matrix, triangle_scale);
+		//model_matrix = glm::rotate(model_matrix, glm::mesh_rotation(1.0f, 0.0f, 0.0f));
 
-		//glm::mat4 view_matrix;
-		//view_matrix = glm::lookAt(glm::vec3(0.0f, 0.0f, 5.0f), //camera positioned here
-		//	glm::vec3(0.0f, 0.0f, 0.0f), //looks at origin
-		//	glm::vec3(0.0f, 1.0f, 0.0f)); //up vector
+		glm::mat4 view_matrix;
+		view_matrix = glm::lookAt(glm::vec3(0.0f, -10.0f, 3.0f), //camera positioned here CAMERA is 5 units back
+			glm::vec3(0.0f, 0.0f, 0.0f), //looks at origin
+			glm::vec3(0.0f, 1.0f, 0.0f)); //up vector
 
-		//glm::mat4 projection_matrix;
-		//projection_matrix = glm::perspective(45.0f, (GLfloat)width / (GLfloat)height, 0.0f, 100.0f);
+		glm::mat4 projection_matrix;
+		projection_matrix = glm::perspectiveFov(45.0f, (GLfloat)windowWidth, (GLfloat)windowHeight, 0.0f, 100.0f);
 
-		//glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(model_matrix)); //broadcast the uniform value to the shaders
-		//glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, glm::value_ptr(view_matrix));
-		//glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection_matrix));
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(model_matrix)); //broadcast the uniform value to the shaders
+		glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, glm::value_ptr(view_matrix));
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection_matrix));
 
-		//glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		//glBindVertexArray(0);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, numvertices); // number of triangles to draw
+		glBindVertexArray(0);
 
+		struct vertex_array {
 
-		unsigned int texture;
-		glGenTextures(1, &texture);
-		//text filtering options
-		glBindTexture(GL_TEXTURE_2D, texture);
-		
-		//import image to be processed
-		int width, height, nrChannels;
-		unsigned char *data = stbi_load("image.jpg", &width, &height, &nrChannels, 0);
-
-		//test image size is within contstraints, else exit
-		if (width > 1024 || height > 1024 || width < 256 || height < 256)
-		{
-			std::cout << "failed to load image because it is not within size constraints. Image is sized:" << height << " " << width << std::endl;
-			exit(0);
-		}
-		if (data)
-		{
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-			std::cout << "successful load" << std::endl;
-			std::cout<< height << std::endl; 
-			std::cout << width << std::endl;
-		}
-		else
-		{
-			std::cout << "failed to load texture" << std::endl;
-		}
-		//free image memory
-		stbi_image_free(data);
-
-
+		};
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
--- vertex.shader
+++ vertex.shader
@@ -10,4 +10,5 @@ uniform mat4 projection_matrix;
 void main()
 {
     gl_Position = projection_matrix * view_matrix * model_matrix * vec4(position.x, position.y, position.z, 1.0);
+	//scaling by whatever is in model matrix, then whatever is in the view matrix, then by the projection matrix
 }
